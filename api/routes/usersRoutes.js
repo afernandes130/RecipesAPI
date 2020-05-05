b569ed9a70df5aa7f7ac983daa15b610545834bd
Adriano Fernandes <afernandes130@hotmail.com>
2020-05-05 10:07:45 -0300

    Nova Funcionalidade - Validação do Json

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -1,10 +1,43 @@
 'use strict';
 module.exports = function(app){
 const user = require('../controllers/usersController');
+const { check, validationResult } = require('express-validator');
 
 app.route('/login')
-    .post(user.Autentication);
+    .post([
+        check('email')
+            .exists().withMessage('E-mail field is requerid')
+            .isEmail().withMessage('Invalid e-mail'),
+        check('password')
+            .exists().withMessage('Password field is requerid')
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
+        else{
+            user.Autentication(req, res)
+        }
+    });
 
- app.route('/login/create')
-     .post(user.Create);
+     app.route('/login/create')
+    .post([
+        check('nickname')
+            .exists().withMessage('NickName field is requerid'),
+        check('email')
+            .exists().withMessage('E-mail field is requerid')
+            .isEmail().withMessage('Invalid e-mail'),
+        check('password')
+            .exists().withMessage('Password field is requerid')
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
+        else{
+            user.Create(req, res)
+        }
+    });
 }; 
+
+
